a2196a9412376022386ca49c4b9e73cf0a44ed79
evorick <rickkadlac@gmail.com>
2015-09-02 20:53:24 -0500

    replaced link to php form with formspree link

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/rick@rickkadlac.com",
+                // url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
